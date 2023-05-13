9adea1fef70d3574e238a1c159276682c940f3af
Yip Mann Fai <mannfai.yip@gmail.com>
2023-05-13 17:57:21 +0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Yip Mann-Fai on 13/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
